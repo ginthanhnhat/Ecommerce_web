64f685920e46628868b24377f1882134b223d6ed
Thanh Nhat Tran <nhattran1708@gmail.com>
2024-06-22 16:58:36 +0700

    update code

---
+++ README.md
@@ -0,0 +1,14 @@
+# INTERNATIONAL UNIVERSITY - VIETNAM NATIONAL UNIVERSITY
+# SCHOOL OF COMPUTER SCIENCE AND ENGINEERING
+
+## PROJECT REPORT
+### Course: Web Application Development – IT093IU
+### Lecturer: Assoc. Prof. Nguyen Van Sinh
+
+### Topic: Build a complete website for a store and an admin panel to help store owners manage their website.
+### Github: Ginnit08/Ecommerce_web (github.com)
+
+### Tran Thanh Nhat - ITITIU20266
+### Date: 22/06/2024
+
+Canva: https://www.canva.com/design/DAGI1QP-PP8/Tnp3_T3oS_oJEyuxG1ipkw/edit?utm_content=DAGI1QP-PP8&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton
---
+++ backend/routes/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+
+const router = express.Router()
+
+
+const UserSignUpController = require('../controller/user/userSignUp')
+const userSingInController = require('../controller/user/userSignIn')
+const userDetailsController = require('../controller/user/userDetails')
+const authToken = require('../middelware/authToken')
+const userLogout = require('../controller/user/userLogout')
+const allUsers = require('../controller/user/allUser')
+const updateUser = require('../controller/user/updateUser')
+const UploadProductController = require('../controller/product/uploadProduct')
+const getProductController = require("../controller/product/getProduct")
+const updateProductController = require('../controller/product/updateProduct')
+const getCategoryProduct = require('../controller/product/getCategoryProductOne')
+const getCategoryWiseProduct = require('../controller/product/getCategoryWiseProduct')
+
+router.post("/signup", UserSignUpController)
+router.post("/signin", userSingInController)
+router.get("/user-details", authToken, userDetailsController)
+router.get("/userLogout", userLogout)
+
+// admin panel
+router.get("/all-user", authToken, allUsers)
+router.post("/update-user", authToken, updateUser)
+
+// product
+router.post("/upload-product", authToken, UploadProductController)
+router.get("/get-product", getProductController)
+router.post("/update-product", authToken, updateProductController)
+router.get("/get-categoryProduct", getCategoryProduct)
+router.post("/category-product", getCategoryWiseProduct)
+
+module.exports = router
